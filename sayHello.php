5181e4c7924e3b7f3669a857a4e12ea5f18e63d3
Stas Andreev <43591990+Fenyka@users.noreply.github.com>
2018-12-26 16:20:17 +0300

    Add files via upload

---
+++ sayHello.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+		<title>Some page</title>
+        <link href="../css/phpMM.scc" rel="stylesheet" type="text/css">
+    </head>
+	<body>
+		<div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+		<div id="example">Пример 1.1</div>
+		
+		<div id="content">
+			<h1>Привет, <?php echo &_REQUEST['name']; ?>!</h1>
+			<p>Рады приветствовать вас. Добро пожаловать в начало
+				вашей одиссеи в мире PHP-программирования.</p>
+		</div>
+		
+		<div id="footer"></div>
+	</body>
+</html>
